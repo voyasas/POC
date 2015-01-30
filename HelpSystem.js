cb9be17d46d37575a4e5529dd028cee128091034
Sam Ho <sam.ho@voya.com>
2015-01-30 09:49:21 -0500

    Create HelpSystem.js
    testing js file

---
+++ HelpSystem.js
@@ -0,0 +1,4 @@
+var xmlHelpSystemData = "";
+xmlHelpSystemData += '<?xml version=\"1.0\" encoding=\"utf-8\"?>';
+xmlHelpSystemData += '<WebHelpSystem DefaultUrl=\"content/taskmanagerhelp/overview.htm\" Toc=\"Data/Toc.xml\" Index=\"Data/Index.xml\" Concepts=\"Data/Concepts.xml\" Glossary=\"content/glossary.htm\" SearchDatabase=\"Data/Search.xml\" Alias=\"Data/Alias.xml\" Synonyms=\"Data/Synonyms.xml\" Skin=\"Data/Skininternal/Skin.xml\" Skins=\"Primary,heat,Internal,Voya,ING-bad\" BuildTime=\"06/27/2014 11:41:10 AM\" BuildVersion=\"10.1.0.0\" TargetType=\"WebHelp\" SkinTemplateFolder=\"Skin/\" InPreviewMode=\"false\" MoveOutputContentToRoot=\"false\" MakeFileLowerCase=\"true\" UseCustomTopicFileExtension=\"false\" />';
+CMCXmlParser._FilePathToXmlStringMap.Add('HelpSystem', xmlHelpSystemData);
